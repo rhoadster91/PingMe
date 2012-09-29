771d16b471bf1c3ce73b6bb1a928ea2d0aecf3d8
Girish Kamath <girishk91@gmail.com>
2012-09-29 16:33:19 +0530

    Server architecture fixed
    Two ports to be used on server, one for each, User and Agent. Server
    will be a multithreaded Java program.

--- GenericClasses/src/beit/skn/classes/Message.java
+++ GenericClasses/src/beit/skn/classes/Message.java
@@ -5,4 +5,18 @@ public class Message
 	String senderID = null;
 	Object content = null;
 	
+	public void setSender(String s)
+	{
+		senderID = s;
+	}
+	
+	public void setMessageContent(Object c)
+	{
+		content = c;
+	}
+	
+	public Object getMessageContent()
+	{
+		return content;
+	}
 }
--- PingMeServer/src/beit/skn/pingmeserver/ServerMain.java
+++ PingMeServer/src/beit/skn/pingmeserver/ServerMain.java
@@ -1,13 +1,42 @@
 package beit.skn.pingmeserver;
 
-public class ServerMain {
+import java.io.IOException;
+import java.net.*;
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class ServerMain extends Thread
+{
+	private static ServerSocket userServer = null;
+	private static ServerSocket agentServer = null;	
+	private static final int USER_PORT_NUMBER = 9975;
+	private static final int AGENT_PORT_NUMBER = 9976;
+		
+	@Override
+	public void run()
+	{
+		try 
+		{
+			agentServer = new ServerSocket(AGENT_PORT_NUMBER);
+			System.out.println("New Agent Server up and running, and listening on port " + AGENT_PORT_NUMBER + "...");
+		} 
+		catch (IOException e)		
+		{			
+			e.printStackTrace();
+		}
+		super.run();
 	}
-
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Server initializing...");
+		new ServerMain().start();
+		try 
+		{
+			userServer = new ServerSocket(USER_PORT_NUMBER);
+			System.out.println("New User Server up and running, and listening on port " + USER_PORT_NUMBER + "...");
+		} 
+		catch (IOException e)		
+		{			
+			e.printStackTrace();
+		}
+	}
 }
