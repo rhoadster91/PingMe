25c4a5f747252f5d913ce402402ddb74acd0508e
Girish Kamath <girishk91@gmail.com>
2012-09-30 16:31:21 +0530

    RSA Encryptor class added
    RSA encryption is now possible by simple importing .java file into
    required project.

--- GenericClasses/src/beit/skn/classes/Message.java
+++ GenericClasses/src/beit/skn/classes/Message.java
@@ -4,19 +4,31 @@ public class Message
 {
 	String senderID = null;
 	Object content = null;
+	String control = null;
 	
+	public String getControl() 
+	{
+		return control;
+	}
+
+	public void setControl(String control) 
+	{
+		this.control = control;
+	}
+
 	public void setSender(String s)
 	{
-		senderID = s;
+		this.senderID = s;
 	}
 	
 	public void setMessageContent(Object c)
 	{
-		content = c;
+		this.content = c;
 	}
 	
 	public Object getMessageContent()
 	{
 		return content;
 	}
+	
 }
