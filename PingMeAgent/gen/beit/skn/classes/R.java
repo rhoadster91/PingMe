4c63e4a54350f3ae44c0c0049931621d0f50d9c3
Girish Kamath <girishk91@gmail.com>
2012-10-17 04:20:31 +0530

    Basic UI for Login and Dashboard
    Simple UI for Login and Dashboard has been adopted, will be developed
    soon.

--- PingMeAgent/gen/beit/skn/classes/R.java
+++ PingMeAgent/gen/beit/skn/classes/R.java
@@ -10,16 +10,41 @@ package beit.skn.classes;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int aqua=0x7f050009;
+        public static final int black=0x7f05000f;
+        public static final int blue=0x7f05000d;
+        public static final int fuchsia=0x7f050002;
+        public static final int gray=0x7f050005;
+        public static final int green=0x7f05000c;
+        public static final int lime=0x7f05000a;
+        public static final int maroon=0x7f050008;
+        public static final int navy=0x7f05000e;
+        public static final int olive=0x7f050006;
+        public static final int purple=0x7f050007;
+        public static final int red=0x7f050003;
+        public static final int silver=0x7f050004;
+        public static final int teal=0x7f05000b;
+        public static final int white=0x7f050000;
+        public static final int yellow=0x7f050001;
+    }
     public static final class drawable {
         public static final int icon=0x7f020000;
+        public static final int titlebar=0x7f020001;
+        public static final int titlebutton=0x7f020002;
     }
     public static final class id {
-        public static final int button1=0x7f050005;
-        public static final int linearLayout1=0x7f050004;
-        public static final int textView1=0x7f050000;
-        public static final int textView2=0x7f050002;
-        public static final int txtPass=0x7f050003;
-        public static final int txtUser=0x7f050001;
+        public static final int button1=0x7f06000a;
+        public static final int linearLayout1=0x7f060009;
+        public static final int linearLayout2=0x7f060000;
+        public static final int tBarDashboard=0x7f060002;
+        public static final int tBarLogin=0x7f060005;
+        public static final int tButtonDashboard=0x7f060001;
+        public static final int tButtonLogin=0x7f060004;
+        public static final int textView1=0x7f060003;
+        public static final int textView2=0x7f060007;
+        public static final int txtPass=0x7f060008;
+        public static final int txtUser=0x7f060006;
     }
     public static final class layout {
         public static final int dash=0x7f030000;
