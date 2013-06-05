7ca41fd410d806e54b836c4b02371ae3ce25099f
Girish Kamath <girishk91@gmail.com>
2013-06-06 01:19:20 +0530

    Settings page UI added + bug fix
    The app now shows a "loading" sign before signing in to wait for buttons
    to get registered

--- PingMeUser/src/beit/skn/pingmeuser/DashboardActivity.java
+++ PingMeUser/src/beit/skn/pingmeuser/DashboardActivity.java
@@ -6,12 +6,14 @@ import beit.skn.classes.PushableMessage;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.NotificationManager;
+import android.app.ProgressDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.text.InputType;
@@ -33,7 +35,9 @@ public class DashboardActivity extends Activity
 	Button bPingPlace = null;
 	Button bPingCode = null;
 	Button bPingImage = null;
+	ProgressDialog loading = null;
 	ViewPager myPager;
+	ViewPager.OnPageChangeListener myListener;
 	
 	static IntentFilter ifLocationUpdate; 
 	static BroadcastReceiver brLocationUpdate;
@@ -63,211 +67,248 @@ public class DashboardActivity extends Activity
 		
 		DashboardPagerAdapter adapter = new DashboardPagerAdapter();
         myPager = (ViewPager) findViewById(R.id.mythreepanelpager);
-        myPager.setAdapter(adapter);
-        
+         myPager.setOffscreenPageLimit(3);
 		
 		UserApplication.readSplashboxFromFile(getApplicationContext());
 		
-		ViewPager.OnPageChangeListener myListener = new ViewPager.OnPageChangeListener(){
+		myListener = new ViewPager.OnPageChangeListener(){
 
 			public void onPageScrollStateChanged(int arg0) {
 				// TODO Auto-generated method stub
 				
 			}
 			
-			public void onPageSelected(int arg0) 
-			
+			 
+			public void onPageScrolled(int arg0, float arg1, int arg2)
 			{
 				
 			}
-			public void onPageScrolled(int arg0, float arg1, int arg2)			
+					
+			public void onPageSelected(int arg0)
 			{
-				switch(myPager.getCurrentItem())
+				try
 				{
-				case 1:
-					bPingCab = (Button)findViewById(R.id.buttonPingTaxi);
-					bPingCab.setOnClickListener(new OnClickListener()
+					switch(myPager.getCurrentItem())
 					{
-						public void onClick(View arg0)
-						{
-							Intent startLocMgr = new Intent(getApplicationContext(), UserLocationManagerService.class);
-							startService(startLocMgr);
-							
-							if(myLoc!=null)
-							{
-								Intent sendMessageToService = new Intent();
-								sendMessageToService.setAction(UserApplication.INTENT_TO_SERVICE);
-								PushableMessage m = new PushableMessage(UserApplication.uname, PushableMessage.CONTROL_PUSH);
-								m.setMessageContent(new String("CAB&&&" + myLoc));
-								sendMessageToService.putExtra("pushablemessage", m);
-								sendBroadcast(sendMessageToService);
-								Toast.makeText(getApplicationContext(), "Pinged for a cab.", Toast.LENGTH_LONG).show();
-							}
-							else
-								Toast.makeText(getApplicationContext(), "Waiting to get location update.", Toast.LENGTH_LONG).show();
-						}			
-					});
 					
-					bPingCop = (Button)findViewById(R.id.buttonPingCop);
-					bPingCop.setOnClickListener(new OnClickListener()
-					{
-						public void onClick(View arg0)
+					case 1:					
+						bPingCab = (Button)findViewById(R.id.buttonPingTaxi);
+						bPingCab.setOnClickListener(new OnClickListener()
 						{
-							Intent startLocMgr = new Intent(getApplicationContext(), UserLocationManagerService.class);
-							startService(startLocMgr);
-							
-							if(myLoc!=null)
+							public void onClick(View arg0)
 							{
-							
-								Intent sendMessageToService = new Intent();
-								sendMessageToService.setAction(UserApplication.INTENT_TO_SERVICE);
-								PushableMessage m = new PushableMessage(UserApplication.uname, PushableMessage.CONTROL_PUSH);
-								m.setMessageContent(new String("COP&&&" + myLoc));
-								sendMessageToService.putExtra("pushablemessage", m);
-								sendBroadcast(sendMessageToService);
-								Toast.makeText(getApplicationContext(), "Pinged for the police.", Toast.LENGTH_LONG).show();
-							}
-							else
-								Toast.makeText(getApplicationContext(), "Waiting to get location update.", Toast.LENGTH_LONG).show();
-						}			
-					});
-					
-					bPingRick = (Button)findViewById(R.id.buttonPingAuto);
-					bPingRick.setOnClickListener(new OnClickListener()
-					{
-						public void onClick(View arg0)
-						{
-							Intent startLocMgr = new Intent(getApplicationContext(), UserLocationManagerService.class);
-							startService(startLocMgr);
+								Intent startLocMgr = new Intent(getApplicationContext(), UserLocationManagerService.class);
+								startService(startLocMgr);
+								
+								if(myLoc!=null)
+								{
+									Intent sendMessageToService = new Intent();
+									sendMessageToService.setAction(UserApplication.INTENT_TO_SERVICE);
+									PushableMessage m = new PushableMessage(UserApplication.uname, PushableMessage.CONTROL_PUSH);
+									m.setMessageContent(new String("CAB&&&" + myLoc));
+									sendMessageToService.putExtra("pushablemessage", m);
+									sendBroadcast(sendMessageToService);
+									Toast.makeText(getApplicationContext(), "Pinged for a cab.", Toast.LENGTH_LONG).show();
+								}
+								else
+									Toast.makeText(getApplicationContext(), "Waiting to get location update.", Toast.LENGTH_LONG).show();
+							}			
+						});
 						
-							if(myLoc!=null)
+						bPingCop = (Button)findViewById(R.id.buttonPingCop);
+						bPingCop.setOnClickListener(new OnClickListener()
+						{
+							public void onClick(View arg0)
 							{
-								Intent sendMessageToService = new Intent();
-							
-								sendMessageToService.setAction(UserApplication.INTENT_TO_SERVICE);
-								PushableMessage m = new PushableMessage(UserApplication.uname, PushableMessage.CONTROL_PUSH);
-								m.setMessageContent(new String("RICKSHAW&&&" + myLoc));
-								sendMessageToService.putExtra("pushablemessage", m);
-								sendBroadcast(sendMessageToService);
-								Toast.makeText(getApplicationContext(), "Pinged for a rickshaw.", Toast.LENGTH_LONG).show();
-							}
-							else
-								Toast.makeText(getApplicationContext(), "Waiting to get location update.", Toast.LENGTH_LONG).show();
-						}			
-					});
-					
-					bPingAmb = (Button)findViewById(R.id.buttonPingAmbulance);
-					bPingAmb.setOnClickListener(new OnClickListener()
-					{
-						public void onClick(View arg0)
+								Intent startLocMgr = new Intent(getApplicationContext(), UserLocationManagerService.class);
+								startService(startLocMgr);
+								
+								if(myLoc!=null)
+								{
+								
+									Intent sendMessageToService = new Intent();
+									sendMessageToService.setAction(UserApplication.INTENT_TO_SERVICE);
+									PushableMessage m = new PushableMessage(UserApplication.uname, PushableMessage.CONTROL_PUSH);
+									m.setMessageContent(new String("COP&&&" + myLoc));
+									sendMessageToService.putExtra("pushablemessage", m);
+									sendBroadcast(sendMessageToService);
+									Toast.makeText(getApplicationContext(), "Pinged for the police.", Toast.LENGTH_LONG).show();
+								}
+								else
+									Toast.makeText(getApplicationContext(), "Waiting to get location update.", Toast.LENGTH_LONG).show();
+							}			
+						});
+						
+						bPingRick = (Button)findViewById(R.id.buttonPingAuto);
+						bPingRick.setOnClickListener(new OnClickListener()
 						{
-							Intent startLocMgr = new Intent(getApplicationContext(), UserLocationManagerService.class);
-							startService(startLocMgr);
-							
-							if(myLoc!=null)
+							public void onClick(View arg0)
 							{
-								Intent sendMessageToService = new Intent();
-								sendMessageToService.setAction(UserApplication.INTENT_TO_SERVICE);
-								PushableMessage m = new PushableMessage(UserApplication.uname, PushableMessage.CONTROL_PUSH);
-								m.setMessageContent(new String("AMBULANCE&&&" + myLoc));
-								sendMessageToService.putExtra("pushablemessage", m);
-								sendBroadcast(sendMessageToService);
-								Toast.makeText(getApplicationContext(), "Pinged for an ambulance.", Toast.LENGTH_LONG).show();
-							}
-							else
-								Toast.makeText(getApplicationContext(), "Waiting to get location update.", Toast.LENGTH_LONG).show();
-						}			
-					});
-					
-					bSplash = (Button)findViewById(R.id.dashtosplash);
-					bSplash.setOnClickListener(new OnClickListener()
-					{
-						public void onClick(View arg0)
-						{
-							Intent iSplashbox = new Intent(getApplicationContext(), SplashBoxActivity.class);
-							startActivity(iSplashbox);
-						}			
-					});
-					break;
-					
-					
-				case 2:
-					bPingText = (Button)findViewById(R.id.buttonPingText);
-					bPingText.setOnClickListener(new OnClickListener()
-					{
-						public void onClick(View arg0)
-						{
-							Intent iPingText = new Intent(getApplicationContext(), PingTextActivity.class);
-							startActivity(iPingText);
-						}			
-					});
-					
-					bPingImage = (Button)findViewById(R.id.buttonPingImage);
-					bPingImage.setOnClickListener(new OnClickListener()
-					{
-						public void onClick(View arg0)
-						{
-							Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-							startActivityForResult(cameraIntent, 0);
+								Intent startLocMgr = new Intent(getApplicationContext(), UserLocationManagerService.class);
+								startService(startLocMgr);
 							
-						}			
-					});
-					
-					bPingCode = (Button)findViewById(R.id.buttonPingCode);
-					bPingCode.setOnClickListener(new OnClickListener()
-					{
-						public void onClick(View arg0)
+								if(myLoc!=null)
+								{
+									Intent sendMessageToService = new Intent();
+								
+									sendMessageToService.setAction(UserApplication.INTENT_TO_SERVICE);
+									PushableMessage m = new PushableMessage(UserApplication.uname, PushableMessage.CONTROL_PUSH);
+									m.setMessageContent(new String("RICKSHAW&&&" + myLoc));
+									sendMessageToService.putExtra("pushablemessage", m);
+									sendBroadcast(sendMessageToService);
+									Toast.makeText(getApplicationContext(), "Pinged for a rickshaw.", Toast.LENGTH_LONG).show();
+								}
+								else
+									Toast.makeText(getApplicationContext(), "Waiting to get location update.", Toast.LENGTH_LONG).show();
+							}			
+						});
+						
+						bPingAmb = (Button)findViewById(R.id.buttonPingAmbulance);
+						bPingAmb.setOnClickListener(new OnClickListener()
 						{
-							Intent iShowDevices = new Intent(getApplicationContext(), DeviceListActivity.class);
-							startActivity(iShowDevices);
-						}			
-					});
-					
-					bPingPlace = (Button)findViewById(R.id.buttonPingPlace);
-					bPingPlace.setOnClickListener(new OnClickListener()
-					{
-						public void onClick(View arg0)
+							public void onClick(View arg0)
+							{
+								Intent startLocMgr = new Intent(getApplicationContext(), UserLocationManagerService.class);
+								startService(startLocMgr);
+								
+								if(myLoc!=null)
+								{
+									Intent sendMessageToService = new Intent();
+									sendMessageToService.setAction(UserApplication.INTENT_TO_SERVICE);
+									PushableMessage m = new PushableMessage(UserApplication.uname, PushableMessage.CONTROL_PUSH);
+									m.setMessageContent(new String("AMBULANCE&&&" + myLoc));
+									sendMessageToService.putExtra("pushablemessage", m);
+									sendBroadcast(sendMessageToService);
+									Toast.makeText(getApplicationContext(), "Pinged for an ambulance.", Toast.LENGTH_LONG).show();
+								}
+								else
+									Toast.makeText(getApplicationContext(), "Waiting to get location update.", Toast.LENGTH_LONG).show();
+							}			
+						});
+						
+						bSplash = (Button)findViewById(R.id.dashtosplash);
+						bSplash.setOnClickListener(new OnClickListener()
 						{
-							Intent startLocMgr = new Intent(getApplicationContext(), UserLocationManagerService.class);
-							startService(startLocMgr);						
-							if(myLoc!=null)
-							{								
+							public void onClick(View arg0)
+							{
+								Intent iSplashbox = new Intent(getApplicationContext(), SplashBoxActivity.class);
+								startActivity(iSplashbox);
+							}			
+						});
+						break;
+						
+						
+					case 2:
+						bPingText = (Button)findViewById(R.id.buttonPingText);
+						bPingText.setOnClickListener(new OnClickListener()
+						{
+							public void onClick(View arg0)
+							{
 								Intent iPingText = new Intent(getApplicationContext(), PingTextActivity.class);
-								iPingText.putExtra("Loc", new String("LOC " + myLoc.split("&&&")[0] + " " + myLoc.split("&&&")[1]));
 								startActivity(iPingText);
-							}
-							else
-								Toast.makeText(getApplicationContext(), "Waiting to get location update.", Toast.LENGTH_LONG).show();
-						}			
-					});					
-					bLogout = (Button)findViewById(R.id.buttonLogOut);
-					bLogout.setOnClickListener(new OnClickListener()
-					{
-						public void onClick(View arg0)
+							}			
+						});
+						
+						bPingImage = (Button)findViewById(R.id.buttonPingImage);
+						bPingImage.setOnClickListener(new OnClickListener()
 						{
-							Intent sendMessageToService = new Intent();
-							sendMessageToService.setAction(UserApplication.INTENT_TO_SERVICE);
-							PushableMessage m = new PushableMessage(UserApplication.uname, PushableMessage.CONTROL_LOGOUT);
-							sendMessageToService.putExtra("pushablemessage", m);
-							sendBroadcast(sendMessageToService);
-							Toast.makeText(getApplicationContext(), "Logged out successfully.", Toast.LENGTH_LONG).show();
-							NotificationManager nm = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
-							nm.cancel(R.string.servicetext);
-							finish();
-						}			
-					});
+							public void onClick(View arg0)
+							{
+								Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+								startActivityForResult(cameraIntent, 0);
+								
+							}			
+						});
+						
+						bPingCode = (Button)findViewById(R.id.buttonPingCode);
+						bPingCode.setOnClickListener(new OnClickListener()
+						{
+							public void onClick(View arg0)
+							{
+								Intent iShowDevices = new Intent(getApplicationContext(), DeviceListActivity.class);
+								startActivity(iShowDevices);
+							}			
+						});
+						
+						bPingPlace = (Button)findViewById(R.id.buttonPingPlace);
+						bPingPlace.setOnClickListener(new OnClickListener()
+						{
+							public void onClick(View arg0)
+							{
+								Intent startLocMgr = new Intent(getApplicationContext(), UserLocationManagerService.class);
+								startService(startLocMgr);						
+								if(myLoc!=null)
+								{								
+									Intent iPingText = new Intent(getApplicationContext(), PingTextActivity.class);
+									iPingText.putExtra("Loc", new String("LOC " + myLoc.split("&&&")[0] + " " + myLoc.split("&&&")[1]));
+									startActivity(iPingText);
+								}
+								else
+									Toast.makeText(getApplicationContext(), "Waiting to get location update.", Toast.LENGTH_LONG).show();
+							}			
+						});					
+						bLogout = (Button)findViewById(R.id.buttonLogOut);
+						bLogout.setOnClickListener(new OnClickListener()
+						{
+							public void onClick(View arg0)
+							{
+								Intent sendMessageToService = new Intent();
+								sendMessageToService.setAction(UserApplication.INTENT_TO_SERVICE);
+								PushableMessage m = new PushableMessage(UserApplication.uname, PushableMessage.CONTROL_LOGOUT);
+								sendMessageToService.putExtra("pushablemessage", m);
+								sendBroadcast(sendMessageToService);
+								Toast.makeText(getApplicationContext(), "Logged out successfully.", Toast.LENGTH_LONG).show();
+								NotificationManager nm = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+								nm.cancel(R.string.servicetext);
+								finish();
+							}			
+						});
+						
+						
+						break;
 					
-					
-					break;
-				
+					}
+				}
+				catch(Exception e)
+				{
+					// Do nothing
 				}
 			}
 			
 		};
-		myPager.setOnPageChangeListener(myListener);
-		myPager.setCurrentItem(0);	
-		
+		myPager.setAdapter(adapter);
+		myPager.setCurrentItem(1);
+		loading = ProgressDialog.show(this, "", "Loading...");
+		new InitializePagerTask().execute();
 	}
+	
+	private class InitializePagerTask extends AsyncTask<Void, Void, Void> 
+	{
+@Override
+		protected Void doInBackground(Void... params) 
+		{
+			try 
+			{
+				
+				Thread.sleep(500);
+				loading.dismiss();
+			}
+			catch (InterruptedException e) 
+			{
+				e.printStackTrace();
+			}
+			return null;
+		}
+
+		protected void onPostExecute(Void result) 
+		{
+			myPager.setOnPageChangeListener(myListener);
+			myPager.setCurrentItem(0, false);	
+			myPager.setCurrentItem(1, false);
+			loading.dismiss();
+		}
+	                       
+	} 
 	
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) 
